7ee0fe9777076c2240108680c7bbc3b2467e9233
Cory Forward <cory@linux.com>
2017-12-04 21:20:18 -0700

    Backup

--- datascience/experiments/three.py
+++ datascience/experiments/three.py
@@ -1,3 +1,8 @@
+from configparser import ConfigParser
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
 from datascience.interfaces import Experiment
 
 """
@@ -7,10 +12,64 @@ What keywords are best associated with movies that do well?
 """
 
 SQL_QUERY_KEYWORDS_GENRE = """
-
+SELECT 
+    genre, keyword, count(*) as count
+FROM
+    TitleGenre,
+    Top5000MovieKeywords
+WHERE
+    Top5000MovieKeywords.movieId = TitleGenre.titleId
+GROUP BY genre, keyword
 """
 
 class ExperimentThree(Experiment):
 
+    NROWS = 6
+    NCOLS = 5
+
+    def __init__(self, config: ConfigParser):
+        super().__init__(config)
+        self.figure, self.axis = plt.subplots(nrows=self.NROWS, ncols=self.NCOLS)
+
+    def top_10(self, genre):
+        keywords = self.keyword_totals[genre]
+        sorted_top = sorted(keywords.items(), key=lambda item: item[1], reverse=True)
+        return sorted_top[:10]
+
+    def get_class_probabilities(self, keyword):
+        totals = []
+        for genre in self.keyword_totals.keys():
+            if keyword in self.keyword_totals[genre]:
+                totals.append(self.keyword_totals[genre][keyword])
+            else:
+                totals.append(0)
+        total = sum(totals)
+        return_probabilities = {}
+        for i, genre in zip(range(len(self.keyword_totals.keys())), self.keyword_totals.keys()):
+            if total == 0:
+                return_probabilities[genre] = 0
+            else:
+                return_probabilities[genre] = totals[i] / total
+        return return_probabilities
+
     def run(self):
-        pass
+        self.keyword_totals = self.query(SQL_QUERY_KEYWORDS_GENRE)
+        top_10_list = {}
+        for genre in self.keyword_totals.keys():
+            top_10_list[genre] = self.top_10(genre)
+
+        row = 0
+        col = 0
+        for genre in self.keyword_totals.keys():
+            action = top_10_list[genre]
+            df = pd.DataFrame(action, columns=["keyword", "count"])
+            df = df[::-1]
+            ax = df.plot(yticks=df.index, kind="barh", ax=self.axis[row][col], title=genre, legend=False)
+            ax.set_yticklabels(df.keyword)
+            if col == self.NCOLS - 1:
+                col = 0
+                row = row + 1
+            else:
+                col = col + 1
+        plt.tight_layout()
+        plt.show()
