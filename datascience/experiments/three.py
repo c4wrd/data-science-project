51d3d73e46d5a2e43f858933337fd57c2eef29eb
Cory Forward <cory@linux.com>
2017-12-04 20:20:37 -0700

    Backup

--- datascience/experiments/three.py
+++ datascience/experiments/three.py
@@ -6,6 +6,10 @@ Can we classify the genre(s) of a movie based on the keywords of the movie?
 What keywords are best associated with movies that do well?
 """
 
+SQL_QUERY_KEYWORDS_GENRE = """
+
+"""
+
 class ExperimentThree(Experiment):
 
     def run(self):
