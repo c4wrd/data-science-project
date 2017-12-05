abbace78ab9130a127e22610538fe0b84bb73629
Cory Forward <cory@linux.com>
2017-12-04 21:55:29 -0700

    fixed sizing of e3 window

--- datascience/experiments/three.py
+++ datascience/experiments/three.py
@@ -29,7 +29,8 @@ class ExperimentThree(Experiment):
 
     def __init__(self, config: ConfigParser):
         super().__init__(config)
-        self.figure, self.axis = plt.subplots(nrows=self.NROWS, ncols=self.NCOLS)
+        self.figure, self.axis = plt.subplots(nrows=self.NROWS, ncols=self.NCOLS, figsize=(20,16))
+        self.figure.canvas.set_window_title("Keyword Occurences in Genres")
 
     def top_10(self, genre):
         keywords = self.keyword_totals[genre]
@@ -52,16 +53,11 @@ class ExperimentThree(Experiment):
                 return_probabilities[genre] = totals[i] / total
         return return_probabilities
 
-    def run(self):
-        self.keyword_totals = self.query(SQL_QUERY_KEYWORDS_GENRE)
-        top_10_list = {}
-        for genre in self.keyword_totals.keys():
-            top_10_list[genre] = self.top_10(genre)
-
+    def plot_genre_keywords(self, top_10_count):
         row = 0
         col = 0
         for genre in self.keyword_totals.keys():
-            action = top_10_list[genre]
+            action = top_10_count[genre]
             df = pd.DataFrame(action, columns=["keyword", "count"])
             df = df[::-1]
             ax = df.plot(yticks=df.index, kind="barh", ax=self.axis[row][col], title=genre, legend=False)
@@ -73,3 +69,23 @@ class ExperimentThree(Experiment):
                 col = col + 1
         plt.tight_layout()
         plt.show()
+
+    def run(self):
+        print("Querying data...")
+        results = self.query(SQL_QUERY_KEYWORDS_GENRE)
+
+        print("Plotting data...")
+        self.keyword_totals = {}
+        for row in results:
+            genre = row['genre']
+            keyword = row['keyword']
+            count = row['count']
+            if genre not in self.keyword_totals:
+                self.keyword_totals[genre] = {}
+            self.keyword_totals[genre][keyword] = count
+
+        top_10_list = {}
+        for genre in self.keyword_totals.keys():
+            top_10_list[genre] = self.top_10(genre)
+
+        self.plot_genre_keywords(top_10_list)
