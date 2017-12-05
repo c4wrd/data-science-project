7350b89c567b509a745b14c66b182d4560430aa2
connerwolfssl <conner@wolfssl.com>
2017-12-04 19:22:52 -0700

    implemented questions 4 and 5

--- datascience/experiments/five.py
+++ datascience/experiments/five.py
@@ -1,10 +1,95 @@
 from datascience.interfaces import Experiment
+import sklearn.decomposition as dec
+import matplotlib.pyplot as plot
+from sklearn.preprocessing import normalize as norm
+from sklearn.cluster import DBSCAN as DB
 
 """
 Is there a correlation between the lifespan in years for a TV show, and the average rating of the TV show?
 """
 
+SQL_QUERY_SUCCESSES = """
+SELECT *
+FROM Top5000Movie
+WHERE Top5000Movie.grossRevenue > Top5000Movie.budget
+"""
+
+SQL_QUERY_FLOPS = """
+SELECT *
+FROM Top5000Movie
+WHERE Top5000Movie.grossRevenue <= Top5000Movie.budget
+"""
+
 class ExperimentFive(Experiment):
 
+    def get_flops(self):
+        results = self.query(SQL_QUERY_FLOPS)
+        keys = list(results[0].keys())[2:]
+        values = [list(dict.values())[2:] for dict in results if None not in list(dict.values())[2:]]
+        values = [val for val in values if val.remove(val[2]) is None] # remove budgets
+        #values = [val for val in values if val.remove(val[1]) is None] # remove grossReveneus
+        keys.remove(keys[2]) # remove budget
+        #keys.remove(keys[1]) # remove grossRevenue
+        return [keys,values]
+
+    def get_successes(self):
+        results = self.query(SQL_QUERY_SUCCESSES)
+        keys = list(results[0].keys())[2:]
+        values = [list(dict.values())[2:] for dict in results if None not in list(dict.values())[2:]]
+        values = [val for val in values if val.remove(val[2]) is None] # remove budgets
+        #values = [val for val in values if val.remove(val[1]) is None] # remove grossReveneus
+        keys.remove(keys[2]) # remove budget
+        #keys.remove(keys[1]) # remove grossRevenue
+        return [keys,values]
+
+    def run_DBSCAN(self, data):
+        print("Running DBSCAN...")
+        print("creating DBSCAN object...")
+        scan = DB(eps=0.0008, #.0005 for 2 clear clusters
+                  min_samples=50, # 30 for 2 clear clusters
+                  metric='euclidean',
+                  metric_params=None,
+                  algorithm='auto',
+                  leaf_size=30,
+                  p=None,
+                  n_jobs=1)
+        print("finding clusters...")
+        scan.fit_predict(data)
+        indexes = scan.core_sample_indices_
+        labels = list(scan.labels_)
+        print("found %s clusters...\ndisplaying clusters..." % (len(set(labels))-1))
+
+        cmap = plot.cm.get_cmap('hsv',len(set(labels)))
+        for label in set(labels):
+            points = [[],[]]
+            if label != -1:
+                for index in indexes:
+                    if labels[index] == label:
+                        points[0].append(data[index][0])
+                        points[1].append(data[index][1])
+                figDB = plot.figure('DBSCAN')
+                plot.scatter(points[0],points[1],c=cmap(label), s=1)
+
     def run(self):
-        pass
+        flops = self.get_flops()
+        flops[1] = norm(flops[1]).tolist()
+        successes = self.get_successes()
+        successes[1] = norm(successes[1]).tolist()
+
+        # PCA AND PLOTS
+        print("Converting %s dimensions to 2 dimensions..." % (len(flops[0])))
+        pca = dec.PCA(n_components=2)
+        new_flops = pca.fit_transform(flops[1])
+        print("Conversion complete...")
+        print("Explained Variance: %s" % (sum(pca.explained_variance_ratio_)))
+        new_successes = pca.fit_transform(successes[1])
+        nf = [[x[0] for x in new_flops],[y[1] for y in new_flops]]
+        ns = [[x[0] for x in new_successes],[y[1] for y in new_successes]]
+        fig1 = plot.figure('PCA DATA')
+        plot.scatter(nf[0],nf[1], color='r', s=1)
+        plot.scatter(ns[0],ns[1],color='b', s=1)
+
+        c_data = new_flops.tolist()+new_successes.tolist()
+        self.run_DBSCAN(c_data)
+
+        plot.show()
--- datascience/experiments/four.py
+++ datascience/experiments/four.py
@@ -1,10 +1,103 @@
 from datascience.interfaces import Experiment
-
+import scipy.stats as stats
+import numpy as np
 """
 Is there a more significant correlation between a movies metascore and revenue or IMDB score and revenue?
 """
 
+SQL_QUERY1 = """
+    SELECT
+        imdbScore as score,grossRevenue as revenue
+    FROM
+        Title
+        JOIN
+        Top5000Movie
+        ON Title.titleId = Top5000Movie.movieId
+    """
+
+SQL_QUERY2 = """
+    SELECT
+        metascore as score,grossRevenue as revenue
+    FROM
+        TitleMetascore
+        JOIN
+        Top5000Movie
+        ON TitleMetascore.titleId = Top5000Movie.movieId
+    """
+
+SQL_QUERY3 = """
+SELECT
+    A.imdbScore as imdb, B.metascore as meta, A.grossRevenue as revenue
+FROM
+(
+		(SELECT
+			imdbScore, grossRevenue, titleId
+		FROM
+			Title
+		JOIN Top5000Movie
+		ON Title.titleId = Top5000Movie.movieId
+		) as A
+
+JOIN
+
+		(SELECT
+			metascore, grossRevenue, titleId
+		FROM
+			TitleMetascore
+		JOIN Top5000Movie
+		ON TitleMetascore.titleId = Top5000Movie.movieId
+        ) as B
+
+ON A.titleId = B.titleId
+)
+WHERE A.grossRevenue IS NOT null
+"""
+
 class ExperimentFour(Experiment):
 
+    # returns a 2D array containing [[score...],[grossRevenue...]]
+    # if SQL_QUERY1 is passed in then score is imdb_scores
+    # if SQL_QUERY2 is passed in then score is metascore
+    def execute_query_pearson(self,q):
+        result = self.query(q)
+        xvals,yvals = [],[]
+        for item in result:
+            if item['revenue'] is not None:
+                xvals.append(float(item['score']))
+                yvals.append(float(item['revenue']))
+        return [xvals,yvals]
+
+    # returns a 2D array containing [[imdb_scores],[metascores],[revenues]]
+    # SQL_QUERY3 should be passed in as the parameter.
+    def execute_query_t_test(self,q = SQL_QUERY2):
+        result = self.query(q)
+        xvals,yvals,rev = [],[],[]
+        for item in result:
+            xvals.append(float(item['imdb']))
+            yvals.append(int(item['meta']/10))
+            rev.append(float(item['revenue']))
+        return [xvals,yvals,rev]
+
+    # returns the imdb peasron correlation, q1 in format [[x],[y]]
+    def imdb_correlation(self,q1):
+        return stats.pearsonr(q1[0],q1[1])
+
+    # returns the metascore peasron correlation, q2in the format [[x],[y]]
+    def metascore_correlation(self,q2):
+        return stats.pearsonr(q2[0],q2[1])
+
+    # selects 100 samples of imdb_scores and revenues and
+    # selects 100 samples of metascores and revenues
+    # returns the two sample t-test statistics of the two samples
+    def t_test(self,q1,q2):
+         a = np.random.choice(q1, size=100, replace=False)
+         b = np.random.choice(q2, size=100, replace=False)
+         return stats.ttest_ind(a,b, equal_var=False)
+
     def run(self):
-        pass
+        q1_results = self.execute_query_pearson(SQL_QUERY1)
+        q2_results = self.execute_query_pearson(SQL_QUERY2)
+        q3_results = self.execute_query_t_test(SQL_QUERY3)
+        print("IMDB Correlation,P-val: " , self.imdb_correlation(q1_results))
+        print("Meta Correlation,P-val: " , self.metascore_correlation(q2_results))
+        print("T-Test: " , self.t_test(q3_results[0], q3_results[1]))
